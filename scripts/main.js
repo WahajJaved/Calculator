ea7aa89fd3045061dc87ca86b414c00f0a691269
Wahaj Javed <major.payne141@gmail.com>
2018-06-27 18:40:54 +0500

    Interface Design fully complete and added slots for the functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,64 @@
 function init() {
 	let container = document.querySelector('.container');
 	
-	// Adding the screen of the calculator
-	// let screen = document.createElement('div');
-	// screen.id= 'screen';
-	// screen.classList.add('gridScreen');
-	// screen.textContent='5800';
-	// container.appendChild(screen);
-
-	for (let i = 0; i <= 20 ; i++)
+	let currentNum = 7;
+	for (let i = 1; i <= 20 ; i++)
 	{
+		//  Iterating over all the grid buttons and adding initial functionality
 		let current = document.createElement('div');
-		current.classList.add('gridButtons')
-		if (i==0)
-		{
-			current.addEventListener('click',clear);
-			current.textContent = 'AC';
+		current.classList.add('gridButtons');
+		
+
+		if (i == 17) {
+			// TODO : Add floating point functionality
+			current.textContent = ".";
 		}
-		else if (i==1)
-			current.addEventListener('click',clear);
+		else if (i == 19) {
+			// Section for the evaluation of the current expression
+			current.textContent = "=";
+		}
+		else if (i%4 != 0) {
+			// Section for the numbers
+			if (i == 18)
+				currentNum++;
+			current.textContent = currentNum;
+			currentNum++;
+		}
+		else if (i%4 == 0) {
+			currentNum -= 5;
+			current.style.borderLeftColor ="rgba(46, 46, 46, 0.5)";
+
+			// Section to add functionality for the arithematic operators
+			if (i / 4 == 1) {
+				current.textContent = 'C';
+			}
+			else if (i / 4 == 2) {
+				current.textContent = '/';
+			}
+			else if (i / 4 == 3) {
+				current.textContent = '*';
+			}
+			else if (i / 4 == 4) {
+				current.textContent = '-';
+			}
+			else if (i / 4 == 5) {
+				current.textContent = '+';
+			}
+		}
+
+		
+		// Border Aesthetic Corrections
+		if (i%4==0)
+			current.style.borderRightWidth='0px';
+		else if (i % 4 == 1)
+			current.style.borderLeftWidth = '0px';
+
 		container.appendChild(current);
+
 	}
 	console.log(container);
 }
-fucntion clear() {
+function clear() {
 
 }
 
